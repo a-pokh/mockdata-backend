d8e81f0154e09bb38183f77a54a2e88bc515cad8
Anton Pokhylenko <anton.pokhilenko@gmail.com>
2020-07-24 17:29:50 +0300

    Update main generators

--- libs/fakedata/src/data_generator.rs
+++ libs/fakedata/src/data_generator.rs
@@ -1,8 +1,11 @@
 use crate::constants::*;
 use cuid::cuid;
+use fake::faker::internet::raw::*;
 use fake::faker::lorem::en::*;
 use fake::faker::name::raw::*;
 use fake::locales::*;
+use fake::Fake;
+use rand::Rng;
 use uuid::Uuid;
 
 pub struct GeneratorTable {
@@ -84,6 +87,9 @@ pub fn generate(tables: Vec<GeneratorTable>) -> Vec<String> {
                         .iter()
                         .find(|f| f.name == ref_table_pk_field.name)
                         .unwrap();
+                    // if field is unique - pick all ids from ref table
+                    // if not unique - pick random
+                    // in case of unique - ref table should have same count of genereted items
                     values.push(ref_field.values[n as usize].clone());
                 } else {
                     values.push(value.clone());
@@ -114,9 +120,19 @@ pub fn generate(tables: Vec<GeneratorTable>) -> Vec<String> {
 fn generate_data(field: &GeneratorField, count: u32) -> Vec<String> {
     match &field.data_type {
         Some(data_type) => match data_type.as_str() {
-            ID_CUID => return generate_id(data_type, count),
+            ID_CUID => return generate_id(&data_type, count),
+            NUMBER_INTEGER => return generate_number(&data_type, count),
+            NUMBER_FLOAT => return generate_number(&data_type, count),
+            DATE_TIMESTAMP => return generate_date(&data_type, count),
+            BOOLEAN_BOOLEAN => return generate_bool(count),
+            INTERNET_EMAIL => {
+                return fake::vec![String as SafeEmail(EN); count as usize]
+                    .into_iter()
+                    .map(|val| format!(r#"'{}'"#, val))
+                    .collect()
+            }
             _ => {
-                return fake::vec![String as Name(EN); count as usize]
+                return fake::vec![String as Word(); count as usize]
                     .into_iter()
                     .map(|val| format!(r#"'{}'"#, val))
                     .collect()
@@ -134,4 +150,41 @@ fn generate_id(data_type: &String, count: u32) -> Vec<String> {
         }
     }
     values
+}
+
+fn generate_number(data_type: &String, count: u32) -> Vec<String> {
+    let mut values: Vec<String> = Vec::new();
+    for n in 0..count {
+        match data_type.as_str() {
+            NUMBER_INTEGER => values.push(rand::thread_rng().gen_range(0, 1000).to_string()),
+            NUMBER_FLOAT => values.push(
+                rand::thread_rng()
+                    .gen_range::<f32, f32, f32>(0.1, 1000.1)
+                    .to_string(),
+            ),
+            _ => values.push(String::from("null")),
+        }
+    }
+    values
+}
+
+fn generate_date(data_type: &String, count: u32) -> Vec<String> {
+    use fake::faker::chrono::raw::*;
+    match data_type.as_str() {
+        DATE_TIMESTAMP => {
+            return fake::vec![chrono::NaiveDateTime as DateTime(EN); count as usize]
+                .into_iter()
+                .map(|val| format!(r#"'{}'"#, val))
+                .collect()
+        }
+        _ => return vec![String::from("null"); count as usize],
+    }
+}
+
+fn generate_bool(count: u32) -> Vec<String> {
+    use fake::faker::boolean::raw::*;
+    return fake::vec![bool as Boolean(EN, 50); count as usize]
+        .into_iter()
+        .map(|val| format!(r#"{}"#, val))
+        .collect();
 }
